d8b764c9e8b7cc965b2705d9a77fa931fcc48593
BBogle0519 <leetmdgud@gmail.com>
2021-12-30 20:48:05 +0900

    년월일별 통계기능 view 구상

--- pedometer/views.py
+++ pedometer/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import stepCount
@@ -25,6 +25,32 @@ class StepCountViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# 총 걸음수 확인
-
+# 총 걸음수 확인(안드로이드센서에서 처리했음)
 # 걸음수 날짜별 확인(년, 월, 주)
+@permission_classes([AllowAny])
+@api_view(['POST'])
+def StepStatistics(request):
+    if request.method == 'POST':
+        #if request == 년도별
+        #data = fitter (request=사용자 id, 연별로 묶은step수)
+        #return Response(data,status=status.HTTP_200_OK)
+
+        #else if request == 월별
+        #data = fitter (request=사용자 id, 월별로 묶은step수)
+        #return Response(data,status=status.HTTP_200_OK)
+
+        #else if request == 주별
+        #data = fitter (request=사용자 id, 주별로 묶은step수)
+        #return Response(data,status=status.HTTP_200_OK)
+
+        #-------------or------------
+        # 한번에 년월주 데이터 response
+        # year = fitter (request=사용자 id, 연별로 묶은step수)
+        # month = ...
+        # week = ...
+
+        # data= {'년': year, '월' : month, '주' : week}
+
+        #return Response(data,status=status.HTTP_200_OK)
+        pass
+        
